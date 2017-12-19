fc3984f12bc4b9976643bc58dd111b726e0b39ef
macgemini <gememacks@gmail.com>
2017-12-19 15:54:16 +0100

    state changes

--- app/actors/EventsActor.scala
+++ app/actors/EventsActor.scala
@@ -81,12 +81,55 @@ class EventsActor(eventBus: LookupBusImpl, actorSystem: ActorSystem) extends Act
             throw new RuntimeException("Pojo json is wrong")
           }
         }
+
+        printHeapStats()
+        printGCStats()
+        System.out.println("      ")
       }
     }
     case _ => {
       sender() ! OutputMessage("Events: Unknown command or parameters list")
       log.error("Unknown command type")
     }
+  }
+
+  def formatSize(v: Long): String = {
+    if (v < 1024) return v + " B"
+    val z = (63 - java.lang.Long.numberOfLeadingZeros(v)) / 10
+    s"${v.toDouble / (1L << (z * 10))}  ${" KMGTPE".charAt(z)}B"
+  }
+
+  private def printHeapStats(): Unit = {
+    val heapSize = Runtime.getRuntime.totalMemory
+
+    // Get maximum size of heap in bytes. The heap cannot grow beyond this size.// Any attempt will result in an OutOfMemoryException.
+    val heapMaxSize = Runtime.getRuntime.maxMemory
+
+    // Get amount of free memory within the heap in bytes. This size will increase // after garbage collection and decrease as new objects are created.
+    val heapFreeSize = Runtime.getRuntime.freeMemory
+
+    val nativeMemory = sun.misc.SharedSecrets.getJavaNioAccess().getDirectBufferPool().getMemoryUsed();
+
+    System.out.println("Heap Size " + formatSize(heapSize))
+    System.out.println("Heap Max Size " + formatSize(heapMaxSize))
+    System.out.println("Heap Free Size " + formatSize(heapFreeSize))
+    System.out.println("Native Memory " + formatSize(nativeMemory))
+  }
+
+  import java.lang.management.ManagementFactory
+
+  def printGCStats(): Unit = {
+    var totalGarbageCollections: Long = 0
+    var garbageCollectionTime: Long = 0
+    import scala.collection.JavaConversions._
+    for (gc <- ManagementFactory.getGarbageCollectorMXBeans) {
+      val count = gc.getCollectionCount
+      if (count >= 0) totalGarbageCollections += count
+      val time = gc.getCollectionTime
+      if (time >= 0) garbageCollectionTime += time
+    }
+    System.out.println("Total Garbage Collections: " + totalGarbageCollections)
+    System.out.println("Total Garbage Collection Time (ms): " + garbageCollectionTime)
   }
 
 }
